f7a8f69e858173c327b3b54212544c0316b75d24
Joaquin <joaquin@devops.com>
2025-10-16 12:18:55 -0300

    definicion variables y network.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "access_key" {
+    description = "Access Key de Canvas AWS"
     type = string
-    default = ""
 }
 
 variable "secret_key" {
+    description = "Secret Key de Canvas AWS"
     type = string
-    default = ""
 }
 
 variable "token" {
+    description = "Session Token de Canvas AWS"
     type = string
-    default = ""
 }
