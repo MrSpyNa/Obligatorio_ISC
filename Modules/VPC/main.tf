3c4b9c85b1c0815980c96ef761556231fec42db0
Anthony <genexits@gmail.com>
2025-10-18 11:00:12 -0300

    Creacion subnet privada, route_table, asociacion rt-subnet

--- Modules/VPC/main.tf
+++ Modules/VPC/main.tf
@@ -25,3 +25,29 @@ resource "aws_subnet" "public_subnet" { #Subnet para el balanceador
   }
   
 }
+
+resource "aws_subnet" "private_subnet" {
+  count                   = 2
+  vpc_id                  = aws_vpc.vpc-e-commerce.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index + 2)
+  map_public_ip_on_launch = false
+  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+
+  tags = {
+    Name = "subnet-privada-${count.index + 1}"
+  }
+}
+
+resource "aws_route_table" "RT_ecommerce" {
+  vpc_id = aws_vpc.vpc-e-commerce.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Geteway.id
+
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.RT_ecommerce.id
+}
